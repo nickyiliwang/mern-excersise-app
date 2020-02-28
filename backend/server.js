5e91d74158566cb06f6c131062f90b65f61a4387
Nick Wang <nickyiliwang@gmail.com>
2020-02-28 03:01:42 -0500

    working on backend first

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-
+// mongoose
 const mongoose = require("mongoose");
-
+// env
 require("dotenv").config();
 
+// using express
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use(cors());
-app.use(express.json());
-
+// mongoose
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex})
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB db connect established!");
+});
+
+// routes
+const exerciseRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exerciseRouter);
+app.use("/user", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
